61d4c456c2336016af648a6aa02e4cedf703adbe
Tim M <timothy.romero.manas@gmail.com>
2021-03-15 19:06:15 -0400

    Able to fetch user profile once authenticated

--- backend/userController.js
+++ backend/userController.js
@@ -22,3 +22,21 @@ export const authUser = asyncHandler(async (req, res) => {
     res.json({ error: 'Invalid email or password' })
   }
 })
+
+// @desc    Get User Profile
+// @route   GET /api/users/profile
+// @access  Private
+export const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(401)
+    res.json({ error: 'User does not exists' })
+  }
+})
