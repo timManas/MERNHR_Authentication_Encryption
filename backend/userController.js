e18ada1104ff9ecb13b972d71acd0a155d20fd04
Tim M <timothy.romero.manas@gmail.com>
2021-03-16 22:37:07 -0400

    Added backend routing for registering new user

--- backend/userController.js
+++ backend/userController.js
@@ -40,3 +40,32 @@ export const getUserProfile = asyncHandler(async (req, res) => {
     res.json({ error: 'User does not exists' })
   }
 })
+
+export const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+
+  if (user) {
+    res.status(201)
+    res.json({
+      name,
+      email,
+      password,
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid User')
+  }
+})
